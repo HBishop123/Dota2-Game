4d422afdcf74c9ac46d219dea378f27f5ea6015e
Harrison Bishop <harrisonbishsp@gmail.com>
2022-12-15 14:33:42 +0000

    added form to the html and removed form created through DOM

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-const userForm = document.createElement("form");
-const formContainer = document.querySelector(".form-container");
-formContainer.appendChild(userForm);
 
-userForm.innerHTML =
-  "<label for='input-field'>Player 1:</label><input type='text' name='name' placeholder='Meepo'/> <label for='input-field'>Player 2:</label><input type='text' name='name' placeholder='Axe'/> <label for='input-field'>Player 3:</label><input type='text' name='name' placeholder='Brood Mother'/> <label for='input-field'>Player 4:</label><input type='text' name='name' placeholder='Puck'/> <label for='input-field'>Player 5:</label><input type='text' name='name' placeholder='Earth Spirit'/>";
-userForm.style.display = "grid";
-userForm.style.gridTemplateColumns = "100px 170px";
-userForm.style.width = "200px";
-userForm.style.alignSelf = "center";
-userForm.style.justifySelf = "center";
-userForm.style.fontSize = "25px";
-userForm.style.gap = "5px";
+class User {
+  constructor(nameOne, nameTwo, nameThree, nameFour, nameFive) {
+    this.nameOne = nameOne;
+    this.nameTwo = nameTwo;
+    this.nameThree = nameThree;
+    this.nameFour = nameFour;
+    this.nameFive = nameFive;
+  }
+}
+
+const getUserData = (e) => {
+  e.preventDefault();
+  const user = {
+    nameOne: document.getElementById("nameOne").value,
+    nameTwo: document.getElementById("nameTwo").value,
+    nameThree: document.getElementById("nameThree").value,
+    nameFour: document.getElementById("nameFour").value,
+    nameFive: document.getElementById("nameFive").value,
+  };
+
+  const { nameOne, nameTwo, nameThree, nameFour, nameFive } = user;
+  const result = new User(nameOne, nameTwo, nameThree, nameFour, nameFive);
+  document.forms[0].reset();
+  arr.push(result);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("btn").addEventListener("click", getUserData);
+});
+let arr = [];
+console.log(arr);
